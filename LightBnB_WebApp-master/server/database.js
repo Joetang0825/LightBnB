1db18e15110e798f8a2bfb4bc22c7778bfbafff3
Joe Tang <joetang81@gmail.com>
2022-04-18 21:54:47 -0400

    update user functions in database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -20,16 +20,24 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE LOWER(email) = $1
+  `;
+
+  return pool.query(queryString, [email.toLowerCase()])
+    .then((result) => {
+      console.log(result.rows);
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -39,7 +47,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1
+  `;
+
+  return pool.query(queryString, [id])
+    .then((result) => {
+      console.log(result.rows);
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -50,10 +74,26 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+
+  const values = [
+    user.name,
+    user.email,
+    user.password
+  ];
+
+  return pool.query(queryString, values)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
 }
 exports.addUser = addUser;
 
@@ -95,9 +135,6 @@ const getAllProperties = (options, limit = 10) => {
     });
 
 };
-
-
-
 
 exports.getAllProperties = getAllProperties;
 
